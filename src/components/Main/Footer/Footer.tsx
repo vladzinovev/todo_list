9ba78791eee0a9a22117edfed08ee9ac646ef8a7
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-02-12 12:15:51 +0300

    added news line

--- src/components/Main/Footer/Footer.tsx
+++ src/components/Main/Footer/Footer.tsx
@@ -1,24 +1,34 @@
-import { NewsService } from "@/services/news.service";
+import { API_URL } from "@/services/news.service";
+import { StoreContext } from "@/store/store";
+import { useContext, useState } from "react";
 import { useQuery } from "react-query";
 
 import styles from "./Footer.module.scss";
+const req = new Request(API_URL);
 
 const Footer = () => {
-  const {
-    isLoading,
-    data: response,
-    error,
-  } = useQuery("news list", () => NewsService.getAll());
+  const { newsOpen } = useContext(StoreContext);
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch(req).then((res) => res.json())
+  );
+
   return (
-    <div className={styles.wrap}>
-      <div className={styles.items_wrap}>
-        <div className={styles.items}>
-          Привет my Name is Vlad i am from london ваввввввв вааааааааааааа
-          ваааааааааааа ваааааааааааааа ппппппппппп оооооооооооооооооо
-          ллллллллллллл абрабабабабаббаба яяяяяяяяяяяя
+    <>
+      {newsOpen && (
+        <div className={styles.wrap}>
+          <div className={styles.items_wrap}>
+            <div className={styles.items}>
+              {isLoading && <p className={styles.status}>...Loading</p>}
+              {error && <p className={styles.status}>{error.message}</p>}
+              {data?.articles.map(
+                (article: { content: string }) =>
+                  article.content + " |----> Next news: "
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 export default Footer;
--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -1,15 +1,7 @@
 import axios from "axios";
 
-const API_URL =
+export const API_URL =
   "https://newsapi.org/v2/everything?q=tesla&" +
   "from=2023-01-11&" +
   "sortBy=publishedAt&" +
   "apiKey=871c399c168b49c883daae36ba6aaa50";
-
-axios.defaults.baseURL = API_URL;
-
-export const NewsService = {
-  async getAll() {
-    return axios.get("/news");
-  },
-};
