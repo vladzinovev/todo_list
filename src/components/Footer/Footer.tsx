07ed7a244f868ee6e422e4cb36e44e6bb1fe037a
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-02-14 12:02:54 +0300

    new fetch

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,50 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { Box, Typography } from "@mui/material";
 
 import { StoreContext } from "@/store/store";
 import styles from "./Footer.module.scss";
 import { INews } from "@/types/types";
+import axios from "axios";
 
-export const API_URL =
-  "https://newsapi.org/v2/everything?" +
-  "q=tesla&" +
-  "from=2023-01-28&" +
-  "sortBy=publishedAt&" +
-  "apiKey=871c399c168b49c883daae36ba6aaa50";
-const req = new Request(API_URL);
+
+const NEWS_URL = 'https://hacker-news.firebaseio.com/v0/newstories.json'
+const ITEM_URL = 'https://hacker-news.firebaseio.com/v0/item/'
+const req = new Request(NEWS_URL);
+export interface INewsItemType {
+  by: string;
+  descendants: number;
+  id: number;
+  kids?: [];
+  score: number;
+  time: number;
+  title?: string;
+  type: string;
+  url: string;
+} 
 
 const Footer = () => {
-  const { newsOpen } = useContext(StoreContext);
+  /* const { newsOpen } = useContext(StoreContext);
 
   const { isLoading, error, data } = useQuery<INews, Error>("repoData", () =>
     fetch(req).then((res) => res.json())
+  ); */
+  const { newsOpen } = useContext(StoreContext);
+  const { isLoading, error, data } = useQuery<INewsItemType[], Error>("repoData", () =>
+    fetch(req).then((res) => res.json())
   );
+  const [posts, setPosts] = useState<INewsItemType[]>([]);
+  
+  useEffect(()=>{
+    data?.map(async (id: INewsItemType) => {
+      await axios
+        .get(`${ITEM_URL}${id}.json`)
+        .then((response) => {
+          setPosts((pos: any) => [...pos, response.data]);
+        })
+    });
+    console.log(posts);
+  },[data])
 
   return (
     <>
@@ -35,9 +60,9 @@ const Footer = () => {
                   {error?.message}
                 </Typography>
               )}
-              {data?.articles?.map(
-                (article: { content: string }) =>
-                  article.content + " |----> Next news: "
+              {posts?.map(
+                (post) =>
+                  post?.title + " |----> Next news: "
               )}
             </Box>
           </Box>
